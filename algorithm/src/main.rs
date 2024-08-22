b64486c338783c931e55c000f158737360951daf
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-08-22 11:28:00 +0200

    Use `sort` in `main`

--- algorithm/src/main.rs
+++ algorithm/src/main.rs
@@ -1,7 +1,8 @@
 use geogroup_algo::*;
 
 fn main() {
-    let r = algo::sort_to_binary_tree(vec![1, 2, 1, 10, 5, 2, 1, 2, 1]);
-
-    println!("{}", algo::flatten(r, 128).print_tree());
+    println!(
+        "{}",
+        algo::sort(vec![1, 2, 1, 10, 5, 2, 1, 2, 1], 128).print_tree()
+    );
 }
