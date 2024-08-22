6611adf8bbb5792d5fa4b13ef78ef7829390e558
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-08-22 11:40:34 +0200

    Simplify `main.rs`

--- algorithm/src/main.rs
+++ algorithm/src/main.rs
@@ -3,6 +3,6 @@ use geogroup_algo::*;
 fn main() {
     println!(
         "{}",
-        algo::sort(vec![1, 2, 1, 10, 5, 2, 1, 2, 1], Params::default()).print_tree()
+        sort(vec![1, 2, 1, 10, 5, 2, 1, 2, 1], Params::default()).print_tree()
     );
 }
