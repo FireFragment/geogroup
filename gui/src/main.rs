6100385ba2f3b9c7b57878404a0348096975297d
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-09-20 22:03:39 +0200

    [GUI] Simple centered welcome screen

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,7 +1,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
 
-use eframe::egui;
+use eframe::*;
+use egui::*;
+use glow::RED;
 
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
@@ -12,38 +14,41 @@ fn main() -> eframe::Result {
     eframe::run_native(
         "My egui App",
         options,
-        Box::new(|cc| Ok(Box::<MyApp>::default())),
+        Box::new(|cc| Ok(Box::<App>::default())),
     )
 }
 
-struct MyApp {
-    name: String,
-    age: u32,
+enum App {
+    WelcomePage(),
 }
 
-impl Default for MyApp {
+impl Default for App {
     fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
-        }
+        Self::WelcomePage()
     }
 }
 
-impl eframe::App for MyApp {
+impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+            egui_extras::StripBuilder::new(ui)
+                .size(egui_extras::Size::remainder())
+                .size(egui_extras::Size::remainder())
+                .vertical(|mut strip| {
+                    strip.cell(|ui| {
+                        ui.with_layout(Layout::bottom_up(Align::Center), |ui| {
+                            ui.label("Locate your photo collection to get started");
+                            ui.heading("Welcome to Geogroup");
+                        });
+                    });
+
+                    strip.cell(|ui| {
+                        ui.vertical_centered(|ui| {
+                            ui.add_space(16.0);
+                            ui.button("🗁 Pick folder");
+                        });
+                    });
+                });
         });
     }
 }
