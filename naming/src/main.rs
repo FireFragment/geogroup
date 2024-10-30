0ccee2346e57f793ae4cc3d4c595332a4656a56d
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-10-30 19:05:22 +0100

    [NAME] Fix `main.rs`

--- naming/src/main.rs
+++ naming/src/main.rs
@@ -6,17 +6,14 @@ use geogroup_naming::*;
 fn main() {
     let p = Point::new(2.12870, 41.40139);
 
-    let oc = get_opencage("native");
-    let cache: PathBuf = env::var("GEOGROUP_CACHE_DIR")
-        .expect("Env var GEOGROUP_CACHE_DIR missing")
-        .into();
+    let geocoder = RevGeocoder::from_env();
 
     for i in 0..100 {
-        let res = reverse_geocode(cache.clone(), &oc, &p).unwrap();
+        let res = geocoder.reverse_geocode(&p).unwrap();
         let place = build_place(res.iter().map(|(k, v)| (k.as_str(), v.to_owned())));
         println!("{i} | {}", format_place(place));
     }
 
-    println!("{:?}", oc.remaining_calls());
+    println!("{:?}", geocoder.opencage.remaining_calls());
     // Some(2494)
 }
