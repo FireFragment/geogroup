78ab14541a9557944cdc6c7e4de2e554c207352c
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-10-30 17:05:28 +0100

    [NAME] Move code to `lib.rs`

--- naming/src/lib.rs
+++ naming/src/lib.rs
@@ -1,14 +1,95 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use address_formatter::Place;
+use geocoding::{GeocodingError, Opencage, Point};
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
+
+pub fn reverse_geocode(
+    mut cache: PathBuf,
+    opencage: &Opencage,
+    point: &Point,
+) -> Result<HashMap<String, String>, GeocodingError> {
+    cache.push(format!("{}_{}", point.0.x, point.0.y));
+
+    println!(" -- Querying cache...");
+    if let Some(ret) = fs::read(&cache)
+        .ok()
+        .map(|cache_contents| Some(bincode::deserialize(&cache_contents[..]).ok()))
+        .flatten()
+        .flatten()
+    {
+        println!(" -- Using cache");
+        Ok(ret)
+    } else {
+        let ret = reverse_geocode_nocache(opencage, point);
+        if let Ok(ret) = &ret {
+            if let Ok(mut file) = File::create(cache) {
+                // TODO: Log the error
+
+                if let Ok(serialized) = bincode::serialize(&ret) {
+                    // TODO: Log the error
+                    file.write_all(&serialized); // TODO: Log the error
+                }
+            }
+        }
+
+        ret
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn reverse_geocode_nocache(
+    opencage: &Opencage,
+    point: &Point,
+) -> Result<HashMap<String, String>, GeocodingError> {
+    println!(" -- Querying OpenCage...");
+    if let [result, ..] = &opencage.reverse_full(point)?.results[..] {
+        Ok(result
+            .components
+            .iter()
+            .filter_map(|(key, value)| Some((key.to_owned(), value.as_str()?.to_owned())))
+            .collect())
+    } else {
+        Err(GeocodingError::Reverse)
     }
+}
+
+pub fn build_place<'a>(data: impl IntoIterator<Item = (&'a str, String)>) -> Place {
+    let mut place = address_formatter::PlaceBuilder::default().build_place(data);
+
+    place[address_formatter::Component::Attention] = None;
+    place[address_formatter::Component::Postcode] = None;
+
+    place
+}
+
+pub fn format_place(place: Place) -> String {
+    address_formatter::FORMATTER.format_with_config(
+            place,
+            address_formatter::Configuration {
+                abbreviate: Some(true),
+                ..Default::default()
+            },
+        )
+        .unwrap()
+        .lines()
+        .collect::<Vec<_>>()
+        .join(", ")
+}
+
+
+/// Returns [Place] with just fields, where both arguments share the same values
+pub fn intersection(a: &Place, b: &Place) -> Place {
+    a.iter()
+    .filter(|(comp, val)| b[*comp] == **val)
+    .filter_map(|(c, v)| {
+        if let Some(rv) = v {
+            Some((c, rv.as_str()))
+        } else {
+            None
+        }
+    })
+    .into()
 }
--- naming/src/main.rs
+++ naming/src/main.rs
@@ -1,14 +1,7 @@
-use std::{
-    collections::HashMap,
-    env,
-    fs::{self, File},
-    hash::Hash,
-    io::Write,
-    iter,
-    path::PathBuf,
-};
+use std::{env, path::PathBuf};
 
-use geocoding::{GeocodingError, Opencage, Point, Reverse};
+use geocoding::{Opencage, Point};
+use geogroup_naming::*;
 
 fn main() {
     let p = Point::new(2.12870, 41.40139);
@@ -21,108 +14,12 @@ fn main() {
         .expect("Env var GEOGROUP_CACHE_DIR missing")
         .into();
 
-    let res = reverse_geocode(cache, &oc, &p).unwrap();
-
-    // "Carrer de Calatrava, 68, 08017 Barcelona, Spain"
-    for (key, val) in res.clone() {
-        println!("{key} => {val}");
+    for i in 0..100 {
+        let res = reverse_geocode(cache.clone(), &oc, &p).unwrap();
+        let place = build_place(res.iter().map(|(k, v)| (k.as_str(), v.to_owned())));
+        println!("{i} | {}", format_place(place));
     }
-
-    println!("\n---------------------------------------\n");
-
-    let mut place = address_formatter::PlaceBuilder::default()
-        .build_place(res.iter().map(|(k, v)| (k.as_str(), v.to_owned())));
-        //.build_place(vec![("continent", String::from("Europe"))]),
-
-    place[address_formatter::Component::Attention] = None;
-    place[address_formatter::Component::Postcode] = None;
-
-    for (key, val) in place.clone() {
-        println!("{key} => {val:?}");
-    }
-
-    println!("\n---------------------------------------\n");
-
-    let formatted = address_formatter::Formatter::default()
-        .format_with_config(
-            place,
-            address_formatter::Configuration {
-                abbreviate: Some(true),
-                ..Default::default()
-            },
-        )
-        .unwrap()
-        .lines()
-        .collect::<Vec<_>>()
-        .join(", ");
-
-    println!("{formatted}");
 
     println!("{:?}", oc.remaining_calls());
     // Some(2494)
-}
-
-fn reverse_geocode(
-    mut cache: PathBuf,
-    opencage: &Opencage,
-    point: &Point,
-) -> Result<HashMap<String, String>, GeocodingError> {
-    cache.push(format!("{}_{}", point.0.x, point.0.y));
-
-    println!(" -- Querying cache...");
-    if let Some(ret) = fs::read(&cache)
-        .ok()
-        .map(|cache_contents| Some(bincode::deserialize(&cache_contents[..]).ok()))
-        .flatten()
-        .flatten()
-    {
-        println!(" -- Using cache");
-        Ok(ret)
-    } else {
-        let ret = reverse_geocode_nocache(opencage, point);
-        if let Ok(ret) = &ret {
-            if let Ok(mut file) = File::create(cache) {
-                // TODO: Log the error
-
-                if let Ok(serialized) = bincode::serialize(&ret) {
-                    // TODO: Log the error
-                    file.write_all(&serialized); // TODO: Log the error
-                }
-            }
-        }
-
-        ret
-    }
-}
-
-fn reverse_geocode_nocache(
-    opencage: &Opencage,
-    point: &Point,
-) -> Result<HashMap<String, String>, GeocodingError> {
-    println!(" -- Querying OpenCage...");
-    if let [result, ..] = &opencage.reverse_full(point)?.results[..] {
-        Ok(result
-            .components
-            .iter()
-            .filter_map(|(key, value)| Some((key.to_owned(), value.as_str()?.to_owned())))
-            .collect())
-    } else {
-        Err(GeocodingError::Reverse)
-    }
-}
-
-use address_formatter::Place;
-
-/// Returns [Place] with just fields, where both arguments share the same values
-pub fn shared(a: &Place, b: &Place) -> Place {
-    a.iter()
-        .filter(|(comp, val)| b[*comp] == **val)
-        .filter_map(|(c, v)| {
-            if let Some(rv) = v {
-                Some((c, rv.as_str()))
-            } else {
-                None
-            }
-        })
-        .into()
 }
