83bde65b31fff70e8aac6f17e9ddbfbcfdd3167d
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-10-31 16:22:40 +0100

    [NAME] Place pruning

--- naming/src/logic.rs
+++ naming/src/logic.rs
@@ -16,16 +16,22 @@ fn get_place_of_hiearchy<LeafData>(hiearchy: &PlaceAnnotatedHiearchy<LeafData>) 
 }
 
 /// [`Place`] doesn't implement [`Clone`] for some reason...
-pub fn place_to_owned(pl: &Place) -> Place {
-    Place::from(
-        pl.iter()
-            .filter_map(|(k, v)| v.as_ref().map(|v| (k, v.as_str()))),
-    )
-}
 
-/// Returns [Place] with just fields, where both arguments share the same values
-pub fn intersection(a: &Place, b: &Place) -> Place {
-    a.iter()
+
+/// Operations on places
+pub mod place_op {
+    use super::*;
+
+    pub fn to_owned(pl: &Place) -> Place {
+        Place::from(
+            pl.iter()
+            .filter_map(|(k, v)| v.as_ref().map(|v| (k, v.as_str()))),
+        )
+    }
+
+    /// Returns [Place] with just fields, where both arguments share the same values
+    pub fn intersection(a: &Place, b: &Place) -> Place {
+        a.iter()
         .filter(|(comp, val)| b[*comp] == **val)
         .filter_map(|(c, v)| {
             if let Some(rv) = v {
@@ -35,6 +41,30 @@ pub fn intersection(a: &Place, b: &Place) -> Place {
             }
         })
         .into()
+    }
+
+    /// Removes the fields present in the second argument from the first argument
+    pub fn subtract(a: &mut Place, b: &Place) {
+        for (component, _) in b.iter().filter(|(_, val)| val.is_some()) {
+            a[component] = None;
+        };
+    }
+}
+
+/// This will prune places in the [annotated hiearchy](PlaceAnnotatedHiearchy) so that they don't duplicate information already present in parent groups place
+/// Eg. if there is an item with place `Rome, Piazza del Colloseo` under a group `Rome`, it will remove `Rome` from the items place
+/// and only keep `Piazza del Colloseo`, so that the information about `Rome` is not duplicated between the item and its parent.
+fn prune_hiearchy_places<LeafData>(hiearchy: &mut PlaceAnnotatedHiearchy<LeafData>, parent_place: &Place) {
+    match hiearchy {
+        HiearchyItem::Item((place, _)) => { place_op::subtract(place, parent_place) }
+        HiearchyItem::Group(children, place) => {
+            for child in children {
+                prune_hiearchy_places(child, place)
+            }
+
+            place_op::subtract(place, parent_place)
+        }
+    }
 }
 
 impl RevGeocoder<'_> {
@@ -42,7 +72,12 @@ impl RevGeocoder<'_> {
         &self,
         hiearchy: HiearchyItem<(Point, LeafData)>,
     ) -> HiearchyItem<(String, LeafData), String> {
-        self.annotate_hiearchy_with_full_places(hiearchy)
+        // 1. Annotate
+        let mut annotated_hiearchy = self.annotate_hiearchy_with_full_places(hiearchy);
+        // 2. Prune
+        prune_hiearchy_places(&mut annotated_hiearchy, &Place::default());
+        // 3. Format
+        annotated_hiearchy
             .map_leafs(&|(place, data)| (format_place(place), data))
             .map_group_data(&format_place)
     }
@@ -51,7 +86,7 @@ impl RevGeocoder<'_> {
     ///
     /// ```text
     /// CITY => Rome
-    ///   ->
+    ///   -> CITY => Rome, STREET => Piazza del Colloseo
     /// ```
     fn annotate_hiearchy_with_full_places<LeafData>(
         &self,
@@ -67,8 +102,8 @@ impl RevGeocoder<'_> {
                 if let Some(place_of_self) = named_children
                     .iter()
                     .map(get_place_of_hiearchy)
-                    .map(place_to_owned) // TODO: Not the best performance here because of cloning
-                    .reduce(|a, b| intersection(&a, &b))
+                    .map(place_op::to_owned) // TODO: Not the best performance here because of cloning
+                    .reduce(|a, b| place_op::intersection(&a, &b))
                 {
                     HiearchyItem::Group(named_children, place_of_self)
                 } else {
