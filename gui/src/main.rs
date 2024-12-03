36b99b80e3052036ff0508b34309b38d33c8b7f7
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-12-03 22:12:55 +0100

    [GUI] Start maximized

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -16,7 +16,7 @@ use glow::RED;
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        viewport: egui::ViewportBuilder::default().with_maximized(true),
         ..Default::default()
     };
     eframe::run_native(
