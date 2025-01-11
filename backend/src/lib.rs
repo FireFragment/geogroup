c2f38b14ce782f5b71930c8317e11f835038f5ec
Matěj Snášel <matejsnasel@Adams-MacBook-Air.local>
2025-01-11 11:48:00 +0100

    [APL] Reexport apply from backend

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,6 +1,7 @@
 use std::path::{Path, PathBuf};
 
 pub use geo as geo_lib;
+pub use geogroup_apply as apply;
 pub use geogroup_algo as algorithm;
 pub use geogroup_common::*;
 pub use geogroup_loaders as loaders;
