9cd16610616c9b21846699d197f2224e050f1500
FireFragment <55660550+FireFragment@users.noreply.github.com>
2025-01-18 17:19:12 +0100

    [APL] Create simple applying implementation

--- apply/src/lib.rs
+++ apply/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{os::unix::fs::chown, path::PathBuf};
+
+use geogroup_common::*;
+
+/// Leaf data of a hiearchy to be applied
+pub struct ApplyLeaf {
+    /// What should the file be renamed to
+    pub target_name: String,
+    pub original_path: PathBuf
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn apply_by_copy(hiearchy: HiearchyItem<ApplyLeaf, String>, target_dir: PathBuf) -> std::io::Result<()>  {
+    std::fs::create_dir_all(&target_dir)?;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    match hiearchy {
+        HiearchyItem::Group(children, name) => {
+            for child in children {
+                let mut tg_dir = target_dir.to_owned();
+                tg_dir.push(&name);
+                apply_by_copy(child, tg_dir)?;
+            }
+            Ok(())
+        },
+        HiearchyItem::Item(leaf) => {
+            let mut tg_dir = target_dir.clone();
+            tg_dir.push(leaf.target_name);
+            std::fs::copy(leaf.original_path, tg_dir).map(|_| ())
+        }
     }
 }
