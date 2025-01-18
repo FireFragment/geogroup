9d0c865b1ea2a5bc5cd162b7771990a7afead820
FireFragment <55660550+FireFragment@users.noreply.github.com>
2025-01-18 18:11:36 +0100

    [APL] Make apply use `log` for logging

--- apply/src/lib.rs
+++ apply/src/lib.rs
@@ -6,14 +6,21 @@ use geogroup_common::*;
 pub struct ApplyLeaf {
     /// What should the file be renamed to
     pub target_name: String,
-    pub original_path: PathBuf
+    pub original_path: PathBuf,
 }
 
-pub fn apply_by_copy(hiearchy: HiearchyItem<ApplyLeaf, String>, target_dir: PathBuf) -> std::io::Result<()>  {
+pub fn apply_by_copy(
+    hiearchy: HiearchyItem<ApplyLeaf, String>,
+    target_dir: PathBuf,
+) -> std::io::Result<()> {
     apply_by_copy_inner(hiearchy, target_dir, 0)
 }
 
-fn apply_by_copy_inner(hiearchy: HiearchyItem<ApplyLeaf, String>, target_dir: PathBuf, idx: usize) -> std::io::Result<()>  {
+fn apply_by_copy_inner(
+    hiearchy: HiearchyItem<ApplyLeaf, String>,
+    target_dir: PathBuf,
+    idx: usize,
+) -> std::io::Result<()> {
     if !target_dir.exists() {
         std::fs::create_dir_all(&target_dir)?;
     }
@@ -27,11 +34,18 @@ fn apply_by_copy_inner(hiearchy: HiearchyItem<ApplyLeaf, String>, target_dir: Pa
                 apply_by_copy_inner(child, tg_dir.clone(), idx)?;
             }
             Ok(())
-        },
+        }
         HiearchyItem::Item(leaf) => {
             let mut tg_dir = target_dir.clone();
-            tg_dir.push(format!("{idx} - {}{}", leaf.target_name, leaf.original_path.extension().map(|ext| format!(".{}", ext.to_str().unwrap() /* TODO: Don't unwrap */)).unwrap_or_default()));
-            println!("Writing {}", tg_dir.to_string_lossy());
+            tg_dir.push(format!(
+                "{idx} - {}{}",
+                leaf.target_name,
+                leaf.original_path
+                    .extension()
+                    .map(|ext| format!(".{}", ext.to_str().unwrap() /* TODO: Don't unwrap */))
+                    .unwrap_or_default()
+            ));
+            log::trace!("Writing {}", tg_dir.to_string_lossy());
             std::fs::copy(leaf.original_path, tg_dir).map(|_| ())
         }
     }
