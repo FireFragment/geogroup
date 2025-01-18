1245fe2fdac41622c0206db29d54e7f33e0bb3d8
FireFragment <55660550+FireFragment@users.noreply.github.com>
2025-01-18 17:19:10 +0100

    [APL] Fix

--- apply/src/lib.rs
+++ apply/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{os::unix::fs::chown, path::PathBuf};
+use std::{fmt::format, os::unix::fs::chown, path::PathBuf};
 
 use geogroup_common::*;
 
@@ -10,20 +10,27 @@ pub struct ApplyLeaf {
 }
 
 pub fn apply_by_copy(hiearchy: HiearchyItem<ApplyLeaf, String>, target_dir: PathBuf) -> std::io::Result<()>  {
-    std::fs::create_dir_all(&target_dir)?;
+    apply_by_copy_inner(hiearchy, target_dir, 0)
+}
+pub fn apply_by_copy_inner(hiearchy: HiearchyItem<ApplyLeaf, String>, target_dir: PathBuf, idx: usize) -> std::io::Result<()>  {
+    if !target_dir.exists() {
+        std::fs::create_dir_all(&target_dir)?;
+    }
 
     match hiearchy {
         HiearchyItem::Group(children, name) => {
-            for child in children {
-                let mut tg_dir = target_dir.to_owned();
-                tg_dir.push(&name);
-                apply_by_copy(child, tg_dir)?;
+            let mut tg_dir = target_dir.to_owned();
+            tg_dir.push(format!("{idx} - {name}"));
+
+            for (idx, child) in children.into_iter().enumerate() {
+                apply_by_copy_inner(child, tg_dir.clone(), idx)?;
             }
             Ok(())
         },
         HiearchyItem::Item(leaf) => {
             let mut tg_dir = target_dir.clone();
-            tg_dir.push(leaf.target_name);
+            tg_dir.push(format!("{idx} - {}{}", leaf.target_name, leaf.original_path.extension().map(|ext| format!(".{}", ext.to_str().unwrap() /* TODO: Don't unwrap */)).unwrap_or_default()));
+            println!("Writing {}", tg_dir.to_string_lossy());
             std::fs::copy(leaf.original_path, tg_dir).map(|_| ())
         }
     }
