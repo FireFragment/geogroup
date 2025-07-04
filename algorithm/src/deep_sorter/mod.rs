3e2b9d1b915d34572e473a517d50140b6d30867e
FireFragment <55660550+FireFragment@users.noreply.github.com>
2025-07-04 13:05:54 +0200

    [ALGO] Fix the ligetime error

--- algorithm/src/deep_sorter/mod.rs
+++ algorithm/src/deep_sorter/mod.rs
@@ -1,13 +1,19 @@
-use std::convert::Infallible;
 mod onetime;
 
 use crate::*;
+use geogroup_common::hiearchy::lazy::LazyHiearchyUtils;
 pub use onetime::*;
 
 pub struct DeepSorter<Item: SortableItem> {
     //points: Vec<Item>,
     bintree: BinTree<Item, GroupDataInner<Item>>,
     params: Params,
+}
+
+impl<Item: SortableItem> DeepSorter<Item> {
+    pub fn as_hiearchy(&self) -> &impl for<'a> hiearchy::Lazy<LeafMetadata<'a> = &'a Item> {
+        //&self.bintree.map_groups(|g| ())
+    }
 }
 
 struct GroupDataInner<Item: SortableItem> {
@@ -21,8 +27,6 @@ pub trait SortableItem: Point {
 
     fn get_time(&self) -> Self::Time;
 }
-
-pub trait GetTime {}
 
 /*
 impl<Item: SortableItem> hiearchy::Lazy for DeepSorter<Item> {
@@ -82,22 +86,41 @@ impl<Item: SortableItem> DeepSorter<Item> {
         &mut self,
         time: &Item::Time,
     ) -> Option<&mut BTInnerNode<Item, GroupDataInner<Item>>> {
-        //get_time_group_mut(&mut self.bintree, time)
-
-        todo!()
+        get_time_group_mut(&mut self.bintree, time)
     }
 }
 
-/*fn get_time_end<Item: deep_sorter::SortableItem>(tree: &BTInnerNode<Item, GroupDataInner<Item>>) {
-    match
-}*/
+/// Check if a [`BinTree`] contains the given time.
+///
+/// Same behavior as `get_time_group_mut.is_some()`
+///
+/// Returns false if the `tree` itself is out of range, that is if there are either
+/// no newer items than `time` OR if there are no older items than `time`.
+/// This means that for `BinTree::Leaf` we always return [false].
+fn tree_contains_time<Item: SortableItem>(
+    tree: &BinTree<Item, GroupDataInner<Item>>,
+    time: &Item::Time,
+) -> bool {
+    match tree {
+        BinTree::Leaf(_) => false,
+        BinTree::InnerNode(node) => {
+            !(node.data.lowest_time > *time || node.data.greatest_time < *time)
+        }
+    }
+}
 
+/// Get the smallest group which still contains a given time
+///
 /// Returns [None] if the `tree` itself is out of range, that is if there are either
 /// no newer items than `time` OR if there are no older items than `time`.
+/// This means that for `BinTree::Leaf` we always return [None].
 ///
-/// This means that for `BinTree::Leaf` we always return [None]. May panic on invalid tree,
-/// eg. invalid values
-/*fn get_time_group_mut<'a, Item: SortableItem>(
+/// Returns [None] if and only if the [`tree_contains_time`] returns [`false`] for the same input.
+///
+/// # Panics
+///
+/// May panic on invalid tree, eg. invalid values //TODO: What does this mean??
+fn get_time_group_mut<'a, Item: SortableItem>(
     tree: &'a mut BinTree<Item, GroupDataInner<Item>>,
     time: &Item::Time,
 ) -> Option<&'a mut BTInnerNode<Item, GroupDataInner<Item>>> {
@@ -107,23 +130,24 @@ impl<Item: SortableItem> DeepSorter<Item> {
             if node.data.lowest_time > *time || node.data.greatest_time < *time {
                 return None;
             }
+            // The following code is a bit dirty, because using just recursion into this function and
+            // checking for `None` leads to borrow checker crying
 
-            let [left_child, right_child] = &mut *node.children;
-            let left_res = get_time_group_mut(left_child, time);
-
-            match left_res {
-                Some(l) => Some(l),
-                None => {
-                    let right_res = get_time_group_mut(right_child, time);
-                    match right_res {
-                        Some(r) => Some(r),
-                        None => Some(node),
-                    }
-                }
+            // First check if left child contains a more specific group
+            if tree_contains_time(&node.children[0], time) {
+                return get_time_group_mut(&mut node.children[0], time);
             }
+
+            // Then check if right child contains a more specific group
+            if tree_contains_time(&node.children[1], time) {
+                return get_time_group_mut(&mut node.children[1], time);
+            }
+
+            // If neither child contains it, this node is the smallest containing group
+            Some(node)
         }
     }
-}*/
+}
 
 pub struct GroupRef<'a, Item: SortableItem>(&'a [Item]);
 
@@ -131,6 +155,7 @@ pub struct GroupRef<'a, Item: SortableItem>(&'a [Item]);
 pub struct GroupMetadata {
     pub strength: u8,
 }
+
 /*
 impl<'a, Item: SortableItem + 'a> hiearchy::lazy::GroupRef<'a> for GroupRef<'a, Item> {
     type Hiearchy = DeepSorter<Item>;
